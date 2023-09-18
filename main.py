4e1abf59b504fa6237182e3e1a6cdc1119573c71
Evan Finger <evanfinger@outlook.com>
2023-09-17 23:03:53 -0400

    Prototype

---
+++ main.py
@@ -0,0 +1,50 @@
+from canvasapi import Canvas
+from collections import deque
+
+import canvasGradeBook
+
+API_URL = "https://canvas.vt.edu" # vt canvas API url
+API_KEY = "4511~4s8DAnuzIEWSRTXryIYItXlkWscvzT0ijh9SlxlsnBRlUGaj7I0Ds7AFVaYAOadG" # Evan Finger Canvas API token
+
+canvas = Canvas(API_URL, API_KEY)
+
+c_user = canvas.get_current_user()  # fetches current canvas user
+u_enrollment_list = c_user.get_enrollments()  # fetches current user's enrollment data
+u_course_list = canvas.get_courses()  # fetches the current user's courses
+
+
+for course in u_course_list:
+    print("=============================================")
+    print(course.name)
+    print("=============================================")
+
+    assignment_list = course.get_assignments()
+    assignment_groups = course.get_assignment_groups()
+    for group in assignment_groups:
+        total_group_points = 0
+        max_group_points = 0
+        for assignment in assignment_list:
+            if assignment.assignment_group_id == group.id:
+                try:
+                    max_group_points += assignment.points_possible
+                except:
+                    pass
+
+                submission = assignment.get_submission(canvas.get_current_user().id)
+                if submission.workflow_state == "graded":
+                    try:
+                        total_group_points += submission.score
+                    except:
+                        pass
+                else:
+                    try:
+                        max_group_points -= assignment.points_possible
+                    except:
+                        pass
+
+        print("-------------------------------")
+        print(group.name + " " + str(group.group_weight) + "% of final grade")
+        print(str(total_group_points) + "/" + str(max_group_points))
+
+
+
