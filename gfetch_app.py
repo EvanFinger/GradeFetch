d851780b141b34d5f78ea7831f9838692a719ae4
Evan Finger <105296253+EvanFinger@users.noreply.github.com>
2023-09-25 15:02:06 -0400

    Testing dark mode toggle

--- gfetch_app.py
+++ gfetch_app.py
@@ -1,9 +1,19 @@
 from textual.app import App
-from textual.widgets import Footer, Header
+from textual.widgets import Footer, Header, Button, Input
+
 
 class GradeFetchApp(App):
+
+    BINDINGS = [
+                ("d", "toggle_dark_mode", "Dark Mode")
+            ]
+
     def compose(self):
+
         """Defines Widgets"""
         yield Header()
         yield Footer()
+        yield Input()
 
+    def action_toggle_dark_mode(self):
+        self.dark = not self.dark
