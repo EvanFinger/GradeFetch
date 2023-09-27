88338eaa49af2f8915e860350f6468d16278ec9f
Evan Finger <105296253+EvanFinger@users.noreply.github.com>
2023-09-27 15:19:02 -0400

    Now displays entered user and id in the UI

--- credential_input.py
+++ credential_input.py
@@ -12,28 +12,20 @@ class ProgBars(Static):
 class CredentialDisplay(Static):
     
     def compose(self):
-        yield Label("User", id="name_label")
+        yield Label("\033[1mUser", id="name_label")
         yield Label("-----", id="name_display")
         yield Label("-----", id="uid_display")
 
 class CredInput_Layer2(Static):
     def compose(self):
+        yield Button("LOAD PROFILE", id="load")
         yield Button("FETCH", id="fetch", variant="success")
         yield CredentialDisplay()
         yield ProgBars()
 
 class CredentialInput(Static):
-    API_URL = ""
-    API_TOKEN = ""
     
-    @on(Button.Pressed, "#fetch")
-    @on(Input.Submitted)
-    def getInput(self):
-        input = self.query_one("#url_input", Input)
-        API_URL = input.value
-        input = self.query_one("#token_input", Input)
-        API_TOKEN = input.value
-        self.mount(Label(API_URL + " " + API_TOKEN))
+        
                         
     def compose(self):
         yield Input(value="https://canvas.vt.edu", id="url_input")
--- gfetch_app.py
+++ gfetch_app.py
@@ -3,6 +3,8 @@ from textual.app import App
 from textual.widgets import Footer, Header, Button, Input, Static, ProgressBar, Label
 from credential_input import CredentialInput
 
+from canvasapi import Canvas
+
 
 class MainContainer(Static):
     
@@ -24,5 +26,38 @@ class GradeFetchApp(App):
         yield Footer()
         yield MainContainer()
 
+#######################################
+#######################################
+    
+# Define Keybinds for Application
+    
+#######################################
+#######################################
+    
     def action_toggle_dark_mode(self):
         self.dark = not self.dark
+
+#######################################
+#######################################
+    
+# Handle Events within the Application
+    
+#######################################
+#######################################
+    
+    @on(Button.Pressed, "#load")
+    @on(Input.Submitted)
+    def LoadProfile(self):
+        input = self.query_one("#url_input", Input)
+        API_URL = input.value
+        input = self.query_one("#token_input", Input)
+        API_TOKEN = input.value
+        
+        self.canvas_api = Canvas(API_URL, API_TOKEN)
+            
+        label = self.query_one("#name_display", Label)
+        label.update(self.canvas_api.get_current_user().name)
+        label = self.query_one("#uid_display", Label)
+        label.update(str(self.canvas_api.get_current_user().id))
+        self.mount(Label(self.canvas_api.get_current_user().name + " " + str(self.canvas_api.get_current_user().id)))
+        
